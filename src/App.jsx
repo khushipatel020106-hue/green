48f82ce2fc993c80922fc769d114477a118b7518
khushipatel020106-hue <khushipatel020106@gmail.com>
2026-01-06 17:47:28 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import About from "./pages/About";
 import Vehicles from "./pages/Vehicles";
 import Contact from "./pages/Contact";
 import "./index.css";
-
 import Whychooseus from "./pages/Whychooseus";
+
+
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/vehicles" element={<Vehicles />} />
+      
       <Route path="/whychooseus" element={<Whychooseus />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
